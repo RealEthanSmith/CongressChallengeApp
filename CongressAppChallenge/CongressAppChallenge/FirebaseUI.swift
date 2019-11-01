a83932e116a44fcb431b5242aa2230b0c745eca9
Emmett Shaughnessy <emmett0502@gmail.com>
2019-11-01 00:07:05 -0500

    Same as last commit (extra files)

---
+++ CongressAppChallenge/CongressAppChallenge/FirebaseUI.swift
@@ -0,0 +1,36 @@
+//
+//  FirebaseUI.swift
+//  CongressAppChallenge
+//
+//  Created by Emmett Shaughnessy on 10/31/19.
+//  Copyright © 2019 Emmett Shaughnessy. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseUI
+import GoogleSignIn
+import FBSDKLoginKit
+import FBSDKCoreKit
+
+class FirebaseUI: UIViewController, FUIAuthDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
